f89c42161e3e59fab4b23fd730c004a726dc21cb
Cem Çakmak <alicemcakmak97@gmail.com>
2026-02-03 15:41:14 +0100

    Update version number, Fixes #3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.cemcakmak.hydrotracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.0.3"
+        versionCode = 24
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
